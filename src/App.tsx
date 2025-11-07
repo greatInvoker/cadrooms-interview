73b40af555537e6d537d814a78952ac22986354d
greatInvoker <593597559@qq.com>
2025-11-07 22:10:21 +0800

    feat: 接通supabase的demo

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import { supabase } from "./lib/supabase";
+import type { Scene } from "./types";
 
 function App() {
-  const [count, setCount] = useState(0)
+	const [scenes, setScenes] = useState<Scene[]>([]);
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	async function getScenes() {
+		const { data } = await supabase.from("scenes").select();
+		setScenes(data || []);
+	}
+
+	useEffect(() => {
+		getScenes();
+	}, []);
+
+	return (
+		<ul>
+			{scenes.map((scene) => (
+				<li key={scene.id}>{scene.name}</li>
+			))}
+		</ul>
+	);
 }
 
-export default App
+export default App;
---
+++ src/types/index.ts
@@ -0,0 +1,7 @@
+export interface Scene {
+  id: string;
+  name: string;
+  description: string;
+  assets: string[];
+  updated_at: string;
+}
